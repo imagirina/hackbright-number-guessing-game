c5ff8a6602bcca2e673600350fb6de76fa640a63
agira <imagirina@gmail.com>
2022-03-29 15:06:58 -0700

    Simplified lines

--- game.py
+++ game.py
@@ -1,12 +1,4 @@
 """A number-guessing game."""
-"""
->>> while True:
-...     try:
-...         x = int(input("Please enter a number: "))
-...     except ValueError:
-...         print("Oops!  That was no valid number.  Try again...")
-"""
-
 import random
 
 name = input("Howdy, what's your name? ")
@@ -19,13 +11,11 @@ while secret_number != guess:
     try:   
         guess = int(input("Try to guess my number: "))    
         print("Your guess? " + str(guess))
-        if int(guess) > 100:
-            print("Make sure your guess is between 1-100!")
-        if int(guess) < 1:
-            print ("Make sure your guess is between 1-100!")
-        if int(guess) > secret_number:
+        if guess > 100 or guess < 1:
+            print("Make sure your guess is between 1-100!")        
+        if guess > secret_number and guess < 101:
             print("Your guessed number is too high!")        
-        if int(guess) < secret_number:
+        if guess < secret_number and guess > 0:
             print("You guessed number is too low! ") 
         count = count + 1               
     except ValueError:
