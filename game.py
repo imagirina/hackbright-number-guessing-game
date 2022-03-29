c3f0221467045a03ca3a43af299c85ad55d2cd10
agira <imagirina@gmail.com>
2022-03-29 11:47:06 -0700

    Checks for correct range

--- game.py
+++ game.py
@@ -7,13 +7,14 @@ secret_number = random.randint(1, 100)
 guess = None
 
 while secret_number != guess:
-    guess = int(input("Try to guess my number: "))
+    guess = int(input("Try to guess my number: "))    
     print("Your guess? " + str(guess))
-    if guess > secret_number:
-        print("Your guessed number is too high!")
-        #guess = int(input("What's your next guess?"))
-    if guess < secret_number:
-        print("You guessed number is too low! ")
-        #guess = int(input("What's your next guess?"))
-
+    if int(guess) > 100:
+        print("Make sure your guess is between 1-100!")
+    if int(guess) < 1:
+        print ("Make sure your guess is between 1-100!")
+    if int(guess) > secret_number:
+        print("Your guessed number is too high!")        
+    if int(guess) < secret_number:
+        print("You guessed number is too low! ")            
 print("You guessed right!")
